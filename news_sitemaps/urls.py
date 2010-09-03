752f7dbe6f2a98e63e1ab2f9e4abbfac517ee534
Justin Quick <justquick@gmail.com>
2010-09-03 10:05:24 -0400

    use the registry to populate sitemaps

--- news_sitemaps/urls.py
+++ news_sitemaps/urls.py
@@ -1,46 +1,10 @@
 from django.conf.urls.defaults import *
 from django.conf import settings
-from news_sitemaps import NewsSitemap
 
-sitemaps = {}
+from news_sitemaps import registry
 
-if 'stories' in settings.INSTALLED_APPS:
-    from stories.models import Story
-
-    class StorySitemap(NewsSitemap):
-        limit = 5000
-        def items(self):
-            return Story.published.all()
-            
-        def lastmod(self, obj):
-            return obj.publish_date
-    
-    sitemaps['stories'] = StorySitemap
-    
-if 'django.contrib.flatpages' in settings.INSTALLED_APPS:
-    from django.contrib.flatpages.models import FlatPage
-    
-    class FlatPageSitemap(NewsSitemap):
-        limit = 5000
-        def items(self):
-            return FlatPage.objects.all()
-            
-    sitemaps['flatpages'] = FlatPageSitemap
-
-if 'django.contrib.flatpages' in settings.INSTALLED_APPS:
-    from django.contrib.comments.models import Comment
-    
-    class CommentSitemap(NewsSitemap):
-        limit = 5000
-        def items(self):
-            return Comment.objects.filter(is_public=True,is_removed=False)
-            
-        def lastmod(self, obj):
-            return obj.submit_date
-            
-    sitemaps['comments'] = CommentSitemap
 
 urlpatterns = patterns('news_sitemaps.views',
-    (r'^index\.xml$', 'index', {'sitemaps': sitemaps}),
-    (r'^(?P<section>.+)\.xml', 'news_sitemap', {'sitemaps': sitemaps}),
+    (r'^index\.xml$', 'index', {'sitemaps': registry}),
+    (r'^(?P<section>.+)\.xml', 'news_sitemap', {'sitemaps': registry}),
 )
