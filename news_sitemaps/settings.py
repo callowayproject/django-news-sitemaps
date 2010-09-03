1468a9f8d929df94f0488456baa38fb20619f4be
Justin Quick <justquick@gmail.com>
2010-09-03 10:05:08 -0400

    include google's exceptions to the rule

--- news_sitemaps/settings.py
+++ news_sitemaps/settings.py
@@ -1,5 +1,8 @@
 from django.conf import settings
 
 NAME = getattr(settings, 'PUBLICATION_NAME', 'The Example Times')
-LANG = getattr(settings, 'PUBLICATION_LANGUAGE', getattr(settings, 'LANGUAGE_CODE', 'en')).split('-')[0]
+LANG = getattr(settings, 'PUBLICATION_LANGUAGE', getattr(settings, 'LANGUAGE_CODE', 'en'))
 TZ = getattr(settings, 'PUBLICATION_TIME_ZONE', '-5:00')
+
+if not LANG in ('zh-cn','zh-tw'):
+    LANG = LANG.split('-')[0]
