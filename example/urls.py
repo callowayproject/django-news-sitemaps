f23aa7d941b64ac133777cafb3009e9a7403b3a1
Justin Quick <justquick@gmail.com>
2010-09-03 10:04:07 -0400

    re-did example project to just include comments as example

--- example/urls.py
+++ example/urls.py
@@ -1,11 +1,28 @@
 from django.conf.urls.defaults import *
-from news_sitemaps.urls import urlpatterns as sitemap_patterns
+from news_sitemaps import register, NewsSitemap
 
 from django.contrib import admin
 admin.autodiscover()
 
+from django.contrib.comments.models import Comment
+
+class CommentSitemap(NewsSitemap):
+    limit = 5000
+    def items(self):
+        return Comment.objects.filter(is_public=True,is_removed=False)
+        
+    def lastmod(self, obj):
+        return obj.submit_date
+    
+    def genres(self, obj):
+        return 'UserGenerated, Opinion'
+        
+register(comments=CommentSitemap)
+
+
 urlpatterns = patterns('',
     (r'^admin/', include(admin.site.urls)),
     (r'^comments/', include('django.contrib.comments.urls')),
-) + sitemap_patterns
+    (r'^news-sitemaps/', include('news_sitemaps.urls')),
+)
 
